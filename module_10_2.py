3d6328be37aa6852d035f8c23b0112241c696d77
OlgaV0539 <olv-personal@mail.ru>
2024-11-07 10:24:43 +0700

    Update module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -3,25 +3,25 @@ import  time
 
 
 class Knight(threading.Thread):
-    total_enemies = 100
-
+    
     def __init__(self, name, power):
         super().__init__(name=name)
         self.name = name
         self.power = power
+        self.enemies = 100
 
 
     def run(self):
         print(f"{self.name}, на нас напали!")
         days = 0
-        while Knight.total_enemies > 0:
+        while self.enemies > 0:
             time.sleep(1)
-            if Knight.total_enemies <= self.power:
-                Knight.total_enemies = 0
+            if self.enemies <= self.power:
+                self.enemies = 0
             else:
-                Knight.total_enemies -= self.power
+                self.enemies -= self.power
             days += 1
-            print(f"{self.name} сражается {days} дней..., осталось {Knight.total_enemies} воинов")
+            print(f"\n{self.name} сражается {days} дней..., осталось {Knight.total_enemies} воинов")
         print(f"{self.name} одержал победу спустя {days} дней")
 
 
